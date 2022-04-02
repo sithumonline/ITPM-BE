96ee0022b7ec60bc9ae2fdea66394a9e84a208b3
Sithum Bopitiya <sithumsandeepap@gmail.com>
2022-04-03 05:04:24 +0530

    Add sql script

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -27,3 +27,26 @@ VALUES
 ("teacher 3", "1990-01-01", "address 3", "phone 3"),
 ("teacher 4", "1990-01-01", "address 4", "phone 4"),
 ("teacher 5", "1990-01-01", "address 5", "phone 5");
+
+# ID
+# Name
+# Date
+# Time
+# Link
+CREATE TABLE session(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    date VARCHAR(100) NOT NULL,
+    time VARCHAR(100) NOT NULL,
+    link VARCHAR(256) NOT NULL,
+
+    CONSTRAINT pk_class PRIMARY KEY(id)
+);
+
+INSERT INTO session(name,date,time,link)
+VALUES
+("session 1", "2020-01-01", "time 1", "link 1"),
+("session 2", "2020-01-01", "time 2", "link 2"),
+("session 3", "2020-01-01", "time 3", "link 3"),
+("session 4", "2020-01-01", "time 4", "link 4"),
+("session 5", "2020-01-01", "time 5", "link 5");
