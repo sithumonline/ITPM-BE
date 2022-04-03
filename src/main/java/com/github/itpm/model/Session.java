ce874e8cfb01a41e2b9764d3f6ef7537f10bc2d1
Savindya Dhananjani <savindyadhananjani@gmail.com>
2022-04-03 05:47:12 +0530

    Init session

---
+++ src/main/java/com/github/itpm/model/Session.java
@@ -0,0 +1,89 @@
+package com.github.itpm.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Session {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "name")
+    private String name;
+    @Basic
+    @Column(name = "date")
+    private String date;
+    @Basic
+    @Column(name = "time")
+    private String time;
+    @Basic
+    @Column(name = "link")
+    private String link;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Session session = (Session) o;
+
+        if (id != session.id) return false;
+        if (name != null ? !name.equals(session.name) : session.name != null) return false;
+        if (date != null ? !date.equals(session.date) : session.date != null) return false;
+        if (time != null ? !time.equals(session.time) : session.time != null) return false;
+        if (link != null ? !link.equals(session.link) : session.link != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (time != null ? time.hashCode() : 0);
+        result = 31 * result + (link != null ? link.hashCode() : 0);
+        return result;
+    }
+}
